c4b292d239b42d7935c11df9d0b4c14f654d5e49
Fabio Agrosi <fabio.agrosi@di.unito.it>
2021-10-25 09:44:50 +0200

    [Update] Caricato file esercizi

---
+++ esercizi.py
@@ -0,0 +1,14 @@
+def function(a):
+    print(a)
+
+if __name__ == '__main__':
+    i=10
+    words=[1,2,"ciao",4,5,6]
+    for i in words:
+        print(i)
+
+    dict = {"a": "aa", "b": "bb"}
+    print(dict["a"])
+    print(dict.keys())
+    print(dict.values())
+    function(a=30)
