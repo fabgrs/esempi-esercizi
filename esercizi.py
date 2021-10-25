88eb125ed87b51ecdc1036ade4eb4c1bc0a82a3a
Fabio Agrosi <fabio.grs96@gmail.com>
2021-10-25 11:30:23 +0200

    [Update] bug risolto e migliorato codice

--- esercizi.py
+++ esercizi.py
@@ -6,7 +6,8 @@ if __name__ == '__main__':
     i = 10
     words = [1, 2, "ciao", 4, 5, 6]
     for i, elem in enumerate(words):
-        print("elemento numero ", i, " ",elem)
+        print("Elemento numero ", i, " ",elem)
+        print("ciao")
 
     dict = {"a": "aa", "b": "bb"}
     print(dict["a"])
